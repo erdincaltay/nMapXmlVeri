e8495fee4ed50a4184b5f75c735c7549c0353eb0
erdincaltay <erdincaltay_a@hotmail.com>
2020-04-03 17:46:09 +0300

    nmap ile xml veri

---
+++ nMapXmlVeri/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace nMapXmlVeri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xmldoc = XDocument.Load("out.xml");
+            XName FileStream = XName.Get("host", xmldoc.Root.Name.NamespaceName);
+            foreach (XElement file in xmldoc.Descendants(FileStream))
+            {
+                Console.WriteLine(file.ToString());
+
+                file.Save("outt.xml");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
